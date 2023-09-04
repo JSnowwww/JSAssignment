849339ac52aa6da0aed68672f729145121844ea9
JSnowwww <142068911+JSnowwww@users.noreply.github.com>
2023-09-04 20:09:57 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -45,9 +45,9 @@ function xclose_conv() {
 }
 
 function converter(value) {
-    var convername = ['METER', 'KILOMETER', 'CENTIMETER', 'MILE', 'INCH'];
+    let convername = ['METER', 'KILOMETER', 'CENTIMETER', 'MILE', 'INCH'];
     //           METER
-    var meter = [
+    let meter = [
                  // 0     1       2        3           4
                  ["* 1", "/ 1000", "* 100", "* 0.000621", "* 39.370079"], // 0
                  ["* 1000", "* 1", "* 100000", "* 0.621371", "* 39,370.07874"], // 1
@@ -56,16 +56,16 @@ function converter(value) {
                  ["* 0.0254", "* 0.0000254", "* 2.54", "* 0.0000157828", "* 1"] // 4
                 ];
 
-    var leftbutton = document.getElementsByClassName("btn active");
-    // var rightbutton = document.getElementsByClassName("btn1 active");
-    var leftvalue = leftbutton[0].getAttribute("value");
-    // var rightvalue = rightbutton.val();
-    var rightvalue = value;
+    let leftbutton = document.getElementsByClassName("btn active");
+    // let rightbutton = document.getElementsByClassName("btn1 active");
+    let leftvalue = leftbutton[0].getAttribute("value");
+    // let rightvalue = rightbutton.val();
+    let rightvalue = value;
 
-    var getId_left = 0;
-    var getId_right = 0;
+    let getId_left = 0;
+    let getId_right = 0;
 
-    for (var i = 0; i < convername.length; i++){
+    for (let i = 0; i < convername.length; i++){
         if (leftvalue == convername[i]){
             getId_left = i;
         }
@@ -74,33 +74,33 @@ function converter(value) {
         }
     }
  
-    var multiplier = meter[getId_left][getId_right];
-    var getInput = document.getElementById("num1").value;
-    var total = 0;
-    var operation = multiplier.split(" ");
+    let multiplier = meter[getId_left][getId_right];
+    let getInput = document.getElementById("num1").value;
+    let total = 0;
+    let operation = multiplier.split(" ");
     console.log(operation);
     total = parseFloat(getInput) + operation[0] + parseFloat(operation[1]);
 
     document.getElementById("num2").value = eval(total);
 }
 
-var btnContainer = document.getElementById("listbutton1");
-var btns = btnContainer.getElementsByClassName("btn");
+let btnContainer = document.getElementById("listbutton1");
+let btns = btnContainer.getElementsByClassName("btn");
 
-for (var i = 0; i < btns.length; i++) {
+for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("btn active");
+    let current = document.getElementsByClassName("btn active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
   });
 }
 
-var btnContainer1 = document.getElementById("listbutton2");
-var btns1 = btnContainer1.getElementsByClassName("btn1");
+let btnContainer1 = document.getElementById("listbutton2");
+let btns1 = btnContainer1.getElementsByClassName("btn1");
 
-for (var i = 0; i < btns1.length; i++) {
+for (let i = 0; i < btns1.length; i++) {
     btns1[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("btn1 active");
+    let current = document.getElementsByClassName("btn1 active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
   });
